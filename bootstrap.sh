8c3ec2795f3df039ec43ad7d9df378298c56478b
keepcosmos <keepcosmos@gmail.com>
2016-03-02 22:47:38 +0900

    자바 설치 및 터미널 환경 수정

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,30 @@
 #! /bin/bash
 
-echo "init package manager..."
-apt-get install -y software-properties-common
-apt-get -y update && apt-get -y upgrade
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
 
-sudo locale-gen en_US.UTF-8
+echo "Initializing..."
+apt-get install -y software-properties-common >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
+apt-get -y upgrade >/dev/null 2>&1
 
-apt-get -y install \
-	build-essential \
-        git \
-	curl \
-	wget \
-	libsqlite3-dev \
-	libpq-dev \
-	nginx \
-	nodejs \
-	npm \
-	redis-server \
-	redis-tools \
-	imagemagick
+install 'basic development tool' build-essential curl wget libsqlite3-dev libpq-dev
 
-echo "install postgres"
-apt-get -y install postgresql postgresql-contrib libpq-dev
+install Git git
+install Nginx nginx
+install Nodejs nodejs
+install npm npm
+install Redis redis-server redis-tools
+install 'ImageMagick(Image processor)' imagemagick
+install Java default-jre default-jdk
+
+install PostgreSQL postgresql postgresql-contrib libpq-dev
 sudo -u postgres createuser --superuser vagrant
 
-echo 'source /vagrant/.promptcolor' >>~/.bashrc
+echo 'installing heroku toolbelt'
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
--- rvm.sh
+++ rvm.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
+echo 'source /vagrant/.promptcolor' >>~/.bashrc
+echo 'JAVA_HOME=/usr/lib/jvm/default-java' >>~/.bashrc
+
 echo "install rvm"
-curl -sSL https://get.rvm.io | bash
+curl -sSL https://get.rvm.io | bash  >/dev/null 2>&1
 source /home/vagrant/.rvm/scripts/rvm
-rvm install 2.3.0
-gem update --system
-gem install bundler
+
+rvm install 2.3.0  >/dev/null 2>&1
+gem update --system >/dev/null 2>&1
+gem install bundler >/dev/null 2>&1
+
 rvm rvmrc warning ignore allGemfiles
